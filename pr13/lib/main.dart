97a03ac28ca8ef4358fc65cca89c529dcead0af0
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-12-02 20:18:38 +0300

    .

---
+++ pr13/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pr13/auth/auth_gate.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://aiyydvjkhezcmdlwaflp.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpeXlkdmpraGV6Y21kbHdhZmxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MTA1MzksImV4cCI6MjA0NzQ4NjUzOX0.XTQfN0Jz8FlVisI8CB3ixI9c2xZBNY8d4XKIRaPo_qI',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: AuthGate(),
+    );
+  }
+}
